2b4f51fedc1932e72c374b943ec669c9e47edf4a
jackysmith040 <jackysmith040@gmail.com>
2025-03-17 03:08:13 +0000

    modify scripts

--- .lando/run.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -d "./wp-admin" ]; then
-	wp core download
-fi
-
-if ! $(wp core is-installed); then
-	wp core install --url="http://mycleanspace.local" --title="My Clean Space" --admin_user="admin" --admin_password="password" --admin_email="admin@mycleanspace.local" --skip-email
-fi
